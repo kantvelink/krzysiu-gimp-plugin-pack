kantvelink/krzysiu-gimp-plugin-pack

commit 2fe4bf95d3f82749224473acb1cf21e95cae3b41

menu item name changed

cloudy-background.py
@@ -37,7 +37,7 @@ def krzysiu_cloudy_background(image, drawable, in_bg_color, in_blur_strength, in
     if bg_color_delta == 0:
         bg_color_h = 0
         bg_color_s = 0
-    else:
+        else:
         bg_color_s = bg_color_delta / (1 - abs(2 * bg_color_l - 1)) * 100
 
         if bg_color_max == bg_color_r:
@@ -63,7 +63,7 @@ def krzysiu_cloudy_background(image, drawable, in_bg_color, in_blur_strength, in
 
 register(
     "krzysiu_cloudy_background",
-    "Render cloudy background",
+    "Render cloudy background...",
     "Renders plasma and colorize it",
     "Krzysztof Blachnicki",
     "krzysiu.net",

select-highlight.py
@@ -37,8 +37,6 @@ def highlight_selection(image, drawable, in_bg_color, in_bg_opacity, in_border_c
 
     return
 
-# Registration
-
 register(
     "krzysiu_select_red",    
     "Highlight selection",   
@@ -46,7 +44,7 @@ register(
     "Krzysztof Blachnicki", 
     "krzysiu.net", 
     "December 2015",
-    "<Image>/Filters/Krzysiu/Highlight selection", 
+    "<Image>/Filters/Krzysiu/Highlight selection...", 
     "*", 
     [
         (PF_COLOR, "in_bg_color", "Color of background:", (255, 0, 0)),

commit 38cb46b7454865364dd24a15a1dc7aae4f4b7f0c

the code was broken since 2fe4bf95d3f82749224473acb1cf21e95cae3b41

cloudy-background.py
@@ -37,7 +37,7 @@ def krzysiu_cloudy_background(image, drawable, in_bg_color, in_blur_strength, in
     if bg_color_delta == 0:
         bg_color_h = 0
         bg_color_s = 0
-        else:
+    else:
         bg_color_s = bg_color_delta / (1 - abs(2 * bg_color_l - 1)) * 100
 
         if bg_color_max == bg_color_r:
@@ -63,12 +63,12 @@ def krzysiu_cloudy_background(image, drawable, in_bg_color, in_blur_strength, in
 
 register(
     "krzysiu_cloudy_background",
-    "Render cloudy background...",
+    "Render cloudy background",
     "Renders plasma and colorize it",
     "Krzysztof Blachnicki",
     "krzysiu.net",
     "September 2014",
-    "<Image>/Filters/Krzysiu/Render cloudy background",
+    "<Image>/Filters/Krzysiu/Render cloudy background...",
     "*",
     [
         (PF_COLOR, "in_bg_color", "Color:", (40, 134, 222)),